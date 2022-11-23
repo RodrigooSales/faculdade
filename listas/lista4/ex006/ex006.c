25ab0207d436eea77d15e74ff16c9560990533b2
Rodrigo <teixeirarodrigo2311@gmail.com>
2022-11-23 12:48:46 -0300

    lista 4 atualização

--- listas/lista4/ex006/ex006.c
+++ listas/lista4/ex006/ex006.c
@@ -1,14 +1,17 @@
-// 6. Escreva um programa que copie uma string para outra usando ponteiros. 7120
+// 6. Escreva um programa que copie uma string para outra usando ponteiros.
 
 #include <stdio.h>
 #include <stdlib.h>
 
 #define TAM 100
 
-void copiaString(char *copia, char *cola){
-    *cola = *copia;
-    ++copia;
-    ++cola;
+void copiaString(char *copiar, char *colar){
+    while(*copiar != '\0'){
+        *colar = *copiar;
+        ++copiar;
+        ++colar;
+    }
+    *colar = '\0';
 }
 
 int main(void){
@@ -17,6 +20,8 @@ int main(void){
     printf("Digite a primeira string para ser copiada: ");
     scanf("%s", strin1);
 
+    copiaString(strin1, string2);
     printf("A string foi colada na string de numero 2!\n");
-    printf("String 2: %s", copiaString(strin1, string2));
+    printf("%s\n", string2);
+    return 0;
 }
---
+++ listas/lista4/ex007/ex007.c
@@ -0,0 +1,22 @@
+// 7. Escreva um programa que concatene duas strings utilizando ponteiros.
+#include <stdio.h>
+
+void concatena(char *string1, int t1, char *string2,int t2 ,char *concatenada){
+    
+}
+
+int tamanhoString(char *s){
+    char *ptr = s;
+    while(*s != '\0'){
+        s++;
+    }
+
+    return (int)(s-ptr);
+}
+
+int main(void){
+
+
+
+    return 0;
+}
