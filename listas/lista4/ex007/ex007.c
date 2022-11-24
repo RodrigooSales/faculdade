83afb9b281382dbdbd2611c3321208e9bbcde0a2
Rodrigo <teixeirarodrigo2311@gmail.com>
2022-11-24 08:36:41 -0300

    lista4

--- listas/lista4/ex007/ex007.c
+++ listas/lista4/ex007/ex007.c
@@ -2,7 +2,15 @@
 #include <stdio.h>
 
 void concatena(char *string1, int t1, char *string2,int t2 ,char *concatenada){
-    
+    for(int i=0; i<t1; i++){
+        *concatenada = *string1;
+        ++concatenada;
+    }    
+
+    for(int j=0; j<t2 ; j++ ){
+        *(concatenada + t1) = *string2;
+        ++concatenada;
+    }
 }
 
 int tamanhoString(char *s){
@@ -14,9 +22,26 @@ int tamanhoString(char *s){
     return (int)(s-ptr);
 }
 
+#define TAM 50
+
 int main(void){
+    char string1[TAM], string2[TAM], concatenada[TAM];
+    int t1, t2;
 
+    printf("Digite a primeira string: ");
+    scanf("%s", &string1);
 
+    printf("Digite a segunda string: ");
+    scanf("%s", &string2);
+
+    t1 = tamanhoString(string1);
+    t2 = tamanhoString(string2);
+
+    concatena(string1, t1, string2, t2, concatenada);
+
+    for(int i=0; i<TAM ; i++){
+        printf("%c", concatenada[i]);
+    }
 
     return 0;
 }
