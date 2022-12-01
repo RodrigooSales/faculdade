eb507926039ac5b68627db1b91ea31ad729a807b
Rodrigo <teixeirarodrigo2311@gmail.com>
2022-11-30 21:14:14 -0300

    lista5 q2

--- listas/lista5/ex002/ex002.c
+++ listas/lista5/ex002/ex002.c
@@ -3,7 +3,42 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+void maiorMenor(int *vetor, int tamanho, int *maior, int *menor){
+    int i;
+    *menor = *vetor;
+    *maior = *vetor;
+
+    for(i=1 ; i<tamanho ; i++){
+        if(*menor > *(vetor + i)){
+            *menor = *(vetor + i);
+        }
+        if(*maior < *(vetor + i)){
+            *maior = *(vetor + i);
+        }
+    }
+
+}
 
 int main(int argc, char **argv){
+    int TAM, vetor[TAM], maior, menor;
+
+    TAM = atoi(argv[1]);
+
+    for(int i=0 ; i<TAM ; i++){
+        vetor[i] = rand() % 10;
+    }
     
+    printf("\n\n");
+    for(int i = 0; i < 10; i++)
+        printf("%d ", vetor[i]);
+    printf("\n\n");
+
+    maiorMenor(vetor, TAM, &maior, &menor);
+
+    printf("Maior: %d, seu Endereço: %p\n", maior, &maior);
+    printf("Menor: %d, seu Endereço: %p\n", menor, &menor);
+
+    return 0;
 }
