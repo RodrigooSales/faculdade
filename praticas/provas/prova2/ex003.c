670b63ac06f068f87f5403d44752f1e6000e3331
Rodrigo Sales <90647707+RodrigooSales@users.noreply.github.com>
2022-12-19 17:01:18 -0300

    Update ex003.c

--- praticas/provas/prova2/ex003.c
+++ praticas/provas/prova2/ex003.c
@@ -1,2 +1,21 @@
 // Escreva uma função em C que receba uma string, um caracter c e o ponteiro para um buffer. o buffer deve conter a string recebida a partir
 // da primeira ocorrencia de c. A função tambem deve retornar a quantidade de caracteres validos no buffer.
+int countStringAt(char *text, char c, char *buffer){
+    int i=0, countAt, textLenght = 0;
+
+    while(*(text + i) != "\0"){
+        textLenght++;
+    }
+
+    countAt = 0;
+    while(*(text + i) != c && =< textLenght){
+        countAt++;
+        i++;
+    }
+
+    for(i=0 ; i<countAt ; i++){
+        *(buffer + i) = *(text + i);
+    }
+
+    return countAt;
+}
