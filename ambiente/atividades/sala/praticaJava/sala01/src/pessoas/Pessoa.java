92c189a33ebc45ae764191cd9df3c01691c0153b
Rodrigo <t.rodricksales10@gmail.com>
2023-04-12 07:34:49 -0300

    subindo arquivos da maquina

---
+++ ambiente/atividades/sala/praticaJava/sala01/src/pessoas/Pessoa.java
@@ -0,0 +1,13 @@
+package pessoas;
+
+public class Pessoa {
+    public String nome;
+    public int idade;
+    
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    
+}
