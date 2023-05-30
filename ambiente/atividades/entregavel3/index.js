5df92e99dce109a371841e1d5b3341997f10028f
Rodrigo Sales <t.rodricksales10@gmail.com>
2023-05-30 19:37:09 -0300

    edição entregavel 3

--- ambiente/atividades/entregavel3/index.js
+++ ambiente/atividades/entregavel3/index.js
@@ -14,6 +14,27 @@ app.post('/soma', function (req, res) {
     res.send(`O resultado da soma de ${body.a} e ${body.b} é ${resultado}`);
 });
 
+app.post('/subtrai', function (req, res) {
+    var body = req.body;
+    var resultado = sub(body.a, body.b);
+
+    res.send(`O resultado da subtração de ${body.a} e ${body.b} é ${resultado}`);
+});
+
+app.post('/multiplica', function (req, res) {
+    var body = req.body;
+    var resultado = multi(body.a, body.b);
+
+    res.send(`O resultado da multiplicação de ${body.a} e ${body.b} é ${resultado}`);
+});
+
+app.post('/divide', function (req, res) {
+    var body = req.body;
+    var resultado = div(body.a, body.b);
+
+    res.send(`O resultado da divisão de ${body.a} e ${body.b} é ${resultado}`);
+});
+
 var port = 3001;
 
 app.listen(port, function () {
@@ -23,3 +44,17 @@ app.listen(port, function () {
 function soma(a, b) {
     return a + b;
 }
+
+function sub(a,b){
+    return a - b;
+}
+
+function multi(a,b) {
+    return a * b;
+}
+
+function div(a,b) {
+    return a / b;
+}
+
+
