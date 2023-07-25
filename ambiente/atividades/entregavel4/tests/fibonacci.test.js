e2751993ada31df3b17e82987919a5579768d783
Rodrigo Sales <90647707+RodrigooSales@users.noreply.github.com>
2023-07-25 15:34:57 -0300

    Delete ambiente directory

--- ambiente/atividades/entregavel4/tests/fibonacci.test.js
+++
@@ -1,9 +0,0 @@
-const fibonnaic = require('../js/fibonacci');
-
-test('os 10 primeiros números da sequência de Fibonacci', () => {
-    expect(fibonnaic(10)).toMatchObject([0, 1, 1, 2, 3, 5, 8, 13, 21, 34]);
-});
-
-test('os 20 primeiros números da sequência de Fibonacci', () => {
-    expect(fibonnaic(2)).toMatchObject([0, 1]);
-});
