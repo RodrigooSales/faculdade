7381f4cb3e57715ae2db0b436106aabeda0ae03d
Rodrigo Sales <teixeirarodrigo2311@gmail.com>
2024-08-08 19:29:18 -0300

    semestre 4

--- integracao/pratica2/index.js
+++ integracao/pratica2/index.js
@@ -1,36 +1,71 @@
-const express = require('express')
-const multer = require('multer')
-const csv = require('csv-parser')
-const path = require('path')
-const fs = require('fs')
+// const express = require('express')
+// const multer = require('multer')
+// const csv = require('csv-parser')
+// const path = require('path')
+// const fs = require('fs')
 
-const app = express();
-const port = 3000;
-const upload = multer({dest : './uploads'})
+// const app = express();
+// const port = 3000;
+// const upload = multer({dest : './uploads'})
 
-app.get('/', (req, res) => {
-  res.sendfile(path.join(__dirname, 'index.html'));
-});
+// app.get('/', (req, res) => {
+//   res.sendfile(path.join(__dirname, 'index.html'));
+// });
 
-app.post('/upload', upload.single('csvFile'), (req, res) => {
-  const file = req.file
-  if(!file){
-    res.status(400).send('nenhum arquivo foi anexado')
-    return
-  }
+// app.post('/upload', upload.single('csvFile'), (req, res) => {
+//   const file = req.file
+//   if(!file){
+//     res.status(400).send('nenhum arquivo foi anexado')
+//     return
+//   }
 
-  const results = []
+//   const results = []
 
-  fs.createReadStream(file.path)
-    .pipe(csv())
-    .on('data', (data) => results.push(data))
-    .on('end', () => {
-      console.log(results)
-    })
+//   fs.createReadStream(file.path)
+//     .pipe(csv())
+//     .on('data', (data) => results.push(data))
+//     .on('end', () => {
+//       console.log(results)
+//     })
   
-});
+// });
 
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`App listening at http://localhost:${port}`);
+// });
 
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqps://drydhblv:nI2ZVgy8acFxj73dR7s8tGFa4zJnENZ7@prawn.rmq.cloudamqp.com/drydhblv', function(error0, connection) {
+  if (error0) {
+    throw error0;
+  }
+  connection.createChannel(function(error1, channel) {
+    if (error1) {
+      throw error1;
+    }
+    var exchange = 'a';
+
+    channel.assertExchange(exchange, 'fanout', {
+      durable: false
+    });
+
+    channel.assertQueue('', {
+      exclusive: true
+    }, function(error2, q) {
+      if (error2) {
+        throw error2;
+      }
+      console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q.queue);
+      channel.bindQueue(q.queue, exchange, '');
+
+      channel.consume(q.queue, function(msg) {
+      	if(msg.content) {
+	        console.log(" [x] %s", msg.content.toString());
+	      }
+      }, {
+        noAck: true
+      });
+    });
+  });
+});
---
+++ mobile/kotlin/app/src/main/java/com/rodrigo/atividade_sala_13_05/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.rodrigo.atividade_sala_13_05
+
+class MainActivity {
+
+}
